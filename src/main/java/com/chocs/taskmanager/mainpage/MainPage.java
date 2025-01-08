e1dd0ac5711765106fde38bbd7960bb5ce766dfd
Fernando Hirsch <fernandobhirsch@gmail.com>
2025-01-08 19:37:53 -0300

    Set style at fxml instead of via code

--- src/main/java/com/chocs/taskmanager/mainpage/MainPage.java
+++ src/main/java/com/chocs/taskmanager/mainpage/MainPage.java
@@ -6,13 +6,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class MainPage extends Application {
     public static void createScene(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainPage.class.getResource("main-scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        scene.getStylesheets().add(Objects.requireNonNull(MainPage.class.getResource("mainscene.css")).toExternalForm());
 
         stage.setTitle("Task Manager");
         stage.setScene(scene);
