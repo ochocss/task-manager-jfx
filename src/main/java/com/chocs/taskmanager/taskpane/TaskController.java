3d67523366cf4b16e9596f8e9abca30981996653
Fernando Hirsch <fernandobhirsch@gmail.com>
2025-01-08 19:40:35 -0300

    Fix TaskController

--- src/main/java/com/chocs/taskmanager/taskpane/TaskController.java
+++ src/main/java/com/chocs/taskmanager/taskpane/TaskController.java
@@ -1,27 +1,65 @@
 package com.chocs.taskmanager.taskpane;
 
+import com.chocs.taskmanager.mainpage.MainController;
+import com.chocs.taskmanager.model.Task;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 
 public class TaskController {
 	@FXML
-	public Label id, typeId, subjectId, description, date, daysLeft;
+	private Label typeId, subjectId, description, date, daysLeft;
+	@FXML
+	private Button deleteButton;
 
-	public TaskController() {}
+	private Task task;
+	private Connection conn;
+	private MainController parent;
 
-	public TaskController(String id, String typeId, String subjectId, String description, LocalDate date) throws IOException {
-		this.id.setText(id);
-		this.typeId.setText(typeId);
-		this.subjectId.setText(subjectId);
-		this.description.setText(description);
-		this.date.setText(date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
-		this.daysLeft.setText(String.valueOf(LocalDate.now().getDayOfYear() - date.getDayOfYear()));
-		FXMLLoader.load(Objects.requireNonNull(TaskController.class.getResource("task.fxml")));
+	public TaskController(Task task, Connection conn, MainController parent) {
+		this.task = task;
+		this.conn = conn;
+		this.parent = parent;
 	}
+
+	public void setTask(Task task) {
+		this.task = task;
+	}
+
+	@FXML
+	public void initialize() {
+		deleteButton.setOnAction(t -> delete());
+		try {
+			ResultSet r = conn.createStatement().executeQuery("SELECT Nome FROM TaskTypes WHERE ID_type = " + task.getTypeId());
+			r.next();
+			typeId.setText(r.getString(1));
+
+			r = conn.createStatement().executeQuery("SELECT Nome FROM Subjects WHERE ID_subject = " + task.getSubjectId());
+			r.next();
+			subjectId.setText(r.getString(1));
+
+			description.setText(task.getDescription());
+			date.setText(DateTimeFormatter.ofPattern("dd/MM/yyyy").format(task.getDate()));
+			daysLeft.setText(task.getDate().getDayOfYear() - LocalDate.now().getDayOfYear() + " days left");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	public void delete() {
+		try {
+			conn.createStatement().executeUpdate("DELETE FROM Tasks WHERE ID_task = " + task.getId());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		parent.reload();
+    }
 }
