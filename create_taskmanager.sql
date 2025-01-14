27e5c8d21697f4676d87da545fd09f09d4ca4831
Fernando Hirsch <fernandobhirsch@gmail.com>
2025-01-13 21:58:54 -0300

    Remove unused tables

--- create_taskmanager.sql
+++ create_taskmanager.sql
@@ -3,16 +3,9 @@ drop schema if exists task_manager;
 create schema task_manager;
 use task_manager;
 
-create table Teachers (
-	ID_teacher int not null auto_increment primary key,
-    Nome varchar (50) not null
-);
-
 create table Subjects (
 	ID_subject int not null auto_increment primary key,
-    ID_teacher int not null,
-    Nome varchar (32) not null unique,
-    foreign key (ID_teacher) references Teachers (ID_teacher)
+    Nome varchar (32) not null unique
 );
 
 create table TaskTypes (
@@ -31,15 +24,14 @@ create table Tasks (
 );
 
 insert into TaskTypes values
-	(1, 'Test'), (2, 'Homework'), (3, 'Others');
+                          (1, 'Test'), (2, 'Homework'), (3, 'Others');
 
 -- Insert models
-insert into Teachers values (1, 'Generic Teacher');
-insert into Subjects (ID_teacher, Nome) values
-                                            (1, 'Art'), (1, 'Biology'), (1, 'Chemistry'), (1, 'Databases'), (1, 'English'),
-                                            (1, 'Geography'), (1, 'History'), (1, 'Mathematics'), (1, 'OOP'), (1, 'Philosophy'),
-                                            (1, 'Physical Education'), (1, 'Physics'), (1, 'Portuguese'), (1, 'Sociology'),
-                                            (1, 'Software Engineering');
+insert into Subjects (Nome) values
+                                ('Art'), ('Biology'), ('Chemistry'), ('Databases'), ('English'),
+                                ('Geography'), ('History'), ('Mathematics'), ('OOP'), ('Philosophy'),
+                                ('Physical Education'), ('Physics'), ('Portuguese'), ('Sociology'),
+                                ('Software Engineering');
 
 create user if not exists 'java'@'localhost' identified by 'password';
 grant all on task_manager.* to 'java'@'localhost';
