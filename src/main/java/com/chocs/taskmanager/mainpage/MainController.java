7926222fcd4f0f3fa2103494daf7d19c8e577995
Fernando Hirsch <fernandobhirsch@gmail.com>
2025-01-13 21:55:28 -0300

    Update MainController.java

--- src/main/java/com/chocs/taskmanager/mainpage/MainController.java
+++ src/main/java/com/chocs/taskmanager/mainpage/MainController.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.Objects;
 
 import com.chocs.taskmanager.createtask.CreateController;
